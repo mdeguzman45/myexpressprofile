457242a3f2e8ebaea898509fcea7b562303873ae
mdeguzman45 <marc_tres03@yahoo.com>
2021-05-30 02:06:34 +0800

    Updates for other views

--- routes/index.js
+++ routes/index.js
@@ -24,28 +24,28 @@ router.get('/home', function(req, res, next) {
 
 /* GET About page. */
 router.get('/about', function(req, res, next) {
-	res.render('index', { 
+	res.render('about_me', { 
 		title: 'About Me'
 	});
 });
 
 /* GET Products page. */
 router.get('/projects', function(req, res, next) {
-	res.render('index', { 
+	res.render('projects', { 
 		title: 'Projects'
 	});
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-	res.render('index', { 
+	res.render('services', { 
 		title: 'Services'
 	});
 });
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-	res.render('index', { 
+	res.render('contact', { 
 		title: 'Contact Me'
 	});
 });
