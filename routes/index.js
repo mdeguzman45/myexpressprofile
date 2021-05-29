4d57c6cb9246a53c04f05e69df4f13504458e11b
mdeguzman45 <marc_tres03@yahoo.com>
2021-05-30 02:40:26 +0800

    Updates for about me and contact page form

--- routes/index.js
+++ routes/index.js
@@ -50,4 +50,14 @@ router.get('/contact', function(req, res, next) {
 	});
 });
 
+/* Handle submit form on Contact Page */
+router.post('/submit-contact-form', function(req, res, next) {
+
+	// log the inputs for now
+	console.log("request from submit contact form: ", req.body);
+
+	// redirect visitor to home page
+	res.redirect('/');
+});
+
 module.exports = router;
